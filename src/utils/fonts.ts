189d6e6fb833f5b88199f3ba8d19cd21f303a2ce
nhardiansa <nhardiansa@gmail.com>
2024-02-13 10:11:11 +0700

    feat: poppins fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,6 +1,12 @@
-import { Rubik } from "next/font/google";
+import { Poppins, Rubik } from "next/font/google";
 
 export const rubik = Rubik({
   subsets: ["latin"],
   display: "swap",
+});
+
+export const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["900", "700", "400"],
 });
